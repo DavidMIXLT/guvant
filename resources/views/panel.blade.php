ac9551c8dc6457f7831e4100cf18dff8c080d2d9
David Gomez <davidmix030@gmail.com>
2019-01-14 20:47:50 +0100

    espacio

--- resources/views/panel.blade.php
+++ resources/views/panel.blade.php
@@ -6,7 +6,7 @@
 @section('content')
 
     <br/>
-<a href="#" class="btn btn-primary">Gestion de Usuarios </a><br/><br/>
+    <a href="#" class="btn btn-primary">Gestion de Usuarios </a><br/><br/>
     <a href="panel/products" class="btn btn-primary" >Gestion de Productos </a><br/><br/>
     <a href="#" class="btn btn-primary">Gestion de Pedidos </a>
 @endsection
