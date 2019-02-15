2b8794c442e488d39f52e69ea59eebdb59705e9a
DavidMIXLT <davidmix030@gmail.com>
2019-02-15 08:36:04 +0100

    Seeder para la tabla productos

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -12,15 +12,54 @@ class ProductsTableSeeder extends Seeder
     public function run()
     {
         //
-        for ($i=0; $i < 30; $i++) { 
-            DB::table('products')->insert([
-                'name' => str_random(10),
-                'description' => str_random(10),
-                'stock' => rand(1,15),
-                'created_at' => date('Y-m-d H:m:s'),
-                'updated_at' => date('Y-m-d H:m:s')
-            ]);
-        }
-       
+        DB::table('products')->insert([
+            'name' => 'Coca Cola',
+            'description' => str_random(10),
+            'stock' => rand(1,15),
+            'created_at' => date('Y-m-d H:m:s'),
+            'updated_at' => date('Y-m-d H:m:s')
+        ]);
+        DB::table('products')->insert([
+            'name' => 'Fanta de Naranja',
+            'description' => str_random(10),
+            'stock' => rand(1,15),
+            'created_at' => date('Y-m-d H:m:s'),
+            'updated_at' => date('Y-m-d H:m:s')
+        ]);
+        DB::table('products')->insert([
+            'name' => 'Acuarios',
+            'description' => str_random(10),
+            'stock' => rand(1,15),
+            'created_at' => date('Y-m-d H:m:s'),
+            'updated_at' => date('Y-m-d H:m:s')
+        ]);
+        DB::table('products')->insert([
+            'name' => 'Macarrones',
+            'description' => str_random(10),
+            'stock' => rand(1,15),
+            'created_at' => date('Y-m-d H:m:s'),
+            'updated_at' => date('Y-m-d H:m:s')
+        ]);
+        DB::table('products')->insert([
+            'name' => 'Sal',
+            'description' => str_random(10),
+            'stock' => rand(1,15),
+            'created_at' => date('Y-m-d H:m:s'),
+            'updated_at' => date('Y-m-d H:m:s')
+        ]);
+        DB::table('products')->insert([
+            'name' => 'Aceite',
+            'description' => str_random(10),
+            'stock' => rand(1,15),
+            'created_at' => date('Y-m-d H:m:s'),
+            'updated_at' => date('Y-m-d H:m:s')
+        ]);
+        DB::table('products')->insert([
+            'name' => 'Azucar',
+            'description' => str_random(10),
+            'stock' => rand(1,15),
+            'created_at' => date('Y-m-d H:m:s'),
+            'updated_at' => date('Y-m-d H:m:s')
+        ]);
     }
 }
