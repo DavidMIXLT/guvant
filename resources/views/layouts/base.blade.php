c4a3fa548308bc7dd9ebcc6f806e19d9f3b84380
DavidMIXLT <davidmix030@gmail.com>
2019-03-22 10:40:14 +0100

    Login bar

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -28,7 +28,7 @@
             </div>
 
 
-              <!-- Menu izquierda -->
+            <!-- Menu izquierda -->
             <ul class="list-unstyled components">
 
                 <li class="{{ Request::routeIs('products.index') ? 'active' : '' }}">
@@ -70,18 +70,34 @@
 
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="nav navbar-nav ml-auto">
-                            <li class="nav-item active">
-                                <a class="nav-link" href="#">Page</a>
-                            </li>
+                            <!-- Authentication Links -->
+                            @guest
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Page</a>
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
+                            @if (Route::has('register'))
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Page</a>
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Page</a>
+                            @endif @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true"
+                                    aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                 document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
                             </li>
+                            @endguest
                         </ul>
                     </div>
                 </div>
