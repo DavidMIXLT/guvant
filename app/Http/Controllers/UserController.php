d2b99711ae2a9cde9d0fbf345d8cb5f4f5c6af08
keco1718 <keco1718daw2vib@gmail.com>
2019-05-03 14:08:02 +0200

    delete construct

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,16 +13,7 @@ class UserController extends Controller
         $this->middleware('auth');
     }
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+  
 
 
     public function index()
