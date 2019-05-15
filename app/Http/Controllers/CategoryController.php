a4a7879e28a5804f164b16d8ec68a3ee9d770086
DavidMIXLT <davidmix030@gmail.com>
2019-05-15 21:33:36 +0200

    Fix categories

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -66,7 +66,7 @@ class CategoryController extends Controller
         $category->validate($request);
 
         $category->fill($request->all())->save();
-        $view = view("categories.layouts.tablerow", compact("category"))->render();
+        $view = view("categories.layouts.tableRow", compact("category"))->render();
         return response()->json([
             "status" => "success",
             'html' => $view,
